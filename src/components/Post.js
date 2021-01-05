0a20a17fe85f2cb0402074bffa3644aec007d010
kukuh pribadi <cristiankukuh27@gmail.com>
2021-01-05 12:48:01 +0700

    memperbaiki coloumn

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Post extends Component {
   render() {
     return (
-      <div className="col-4">
+      <div className="col-lg-4">
         <div className="card shadow mb-3 p-3" style={{ borderRadius: "25px" }}>
           <h2 style={{ textTransform: "capitalize" }}>{this.props.title}</h2>
           <hr style={{ border: "solid 3px rgba(0, 0, 0, 0.5)" }} />
